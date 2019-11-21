7782c487e4f7e824f776f2c1d8989b4e091d7a65
ritu-imd <57671224+ritu-imd@users.noreply.github.com>
2019-11-21 21:40:41 +0530

    initial commit (#3)
    * initial commit
    
    * addressing pr comments

--- GitTest/GitTest/Controllers/ValuesController.cs
+++ GitTest/GitTest/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace GitTest.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value5" };
+            return new string[] { "value1", "value2", "value3", "value5" ,"value6", "value7" };
         }
 
         // GET api/values/5
